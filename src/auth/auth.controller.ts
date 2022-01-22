368fe67adb7177a433940f6d45236837978b494b
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-22 19:05:07 +0700

    feat(auth): validate user register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import { RegisterDto } from './dtos/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
-  register(@Body() body) {
+  register(@Body() body: RegisterDto) {
+    if (body.password !== body.password_confirm)
+      throw new BadRequestException('Passwords do not match.');
+
     return this.userService.create(body);
   }
 
