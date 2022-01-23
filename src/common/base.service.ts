382f0bb23b5c04a8f7597b9059378fb53703833b
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-23 15:15:20 +0700

    fix(common): base service bugs

--- src/common/base.service.ts
+++ src/common/base.service.ts
@@ -52,23 +52,17 @@ export abstract class BaseService<T, R extends Repository<T>> {
   async update(id: number, data: DeepPartial<T>) {
     const record = await this.findById(id);
 
-    if (!record)
-      throw new NotFoundException(
-        `${record.constructor.name} #${id} not found.`,
-      );
+    if (!record) throw new NotFoundException(`Record #${id} not found.`);
 
     Object.assign(record, data);
 
-    return this.repository.save(data);
+    return this.repository.save(record);
   }
 
   async delete(id: number) {
     const record = await this.findById(id);
 
-    if (!record)
-      throw new NotFoundException(
-        `${record.constructor.name} #${id} not found.`,
-      );
+    if (!record) throw new NotFoundException(`Record #${id} not found.`);
 
     return this.repository.remove(record);
   }
