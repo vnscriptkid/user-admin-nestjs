e53fc082118621491165996bd7573a13ed58a50a
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-23 18:47:42 +0700

    feat(order): export orders to csv

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -3,10 +3,14 @@ import {
   ClassSerializerInterceptor,
   Controller,
   Get,
+  Post,
   Query,
+  Res,
   UseInterceptors,
 } from '@nestjs/common';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Parser } from 'json2csv';
+import { Response } from 'express';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('orders')
@@ -16,5 +20,45 @@ export class OrderController {
   @Get()
   all(@Query() { pageNumber, pageSize }: PaginationDto) {
     return this.orderService.paginate(pageNumber, pageSize, ['order_items']);
+  }
+
+  @Post('export')
+  async exportCsv(@Res() res: Response) {
+    const parser = new Parser({
+      fields: ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity'],
+    });
+
+    const orders = await this.orderService.all({ relations: ['order_items'] });
+
+    const json = [];
+
+    orders.forEach((order) => {
+      json.push({
+        ID: order.id,
+        Name: order.name,
+        Email: order.email,
+        'Product Title': '',
+        Price: '',
+        Quantity: '',
+      });
+
+      order.order_items.forEach((item) => {
+        json.push({
+          ID: '',
+          Name: '',
+          Email: '',
+          'Product Title': item.product_title,
+          Price: item.price,
+          Quantity: item.quantity,
+        });
+      });
+    });
+
+    const csv = parser.parse(json);
+
+    res.header('Content-Type', 'text/csv');
+    res.attachment('orders.csv');
+
+    return res.send(csv);
   }
 }
