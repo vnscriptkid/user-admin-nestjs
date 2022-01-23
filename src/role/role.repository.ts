820064d47a996935c18dc2910d6f302ee0f8c3c9
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-23 15:17:03 +0700

    refactor(role): clean up

--- src/role/role.repository.ts
+++ src/role/role.repository.ts
@@ -2,6 +2,5 @@ import { Injectable } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { Role } from './models/role.entity';
 
-@Injectable()
 @EntityRepository(Role)
 export class RoleRepository extends Repository<Role> {}
